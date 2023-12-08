dcdcd9039b0672dd6ff86e0f6e4345b797fa1a98
ChanghyeonO <ckdgus5189@gmail.com>
2023-12-08 23:57:20 +0900

    feat: 제품 소개 컴포넌트 완성

--- src/components/PostContent/PostMainContent/PostMainContentStyle.ts
+++ src/components/PostContent/PostMainContent/PostMainContentStyle.ts
@@ -3,24 +3,3 @@ import styled from "styled-components";
 export const Container = styled.div`
   min-height: 100vh;
 `;
-
-export const NewListingArea = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 100px;
-  width: 100%;
-  height: auto;
-  max-width: 1200px;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  padding: 100px;
-`;
-
-export const NewItem = styled.div`
-  width: calc(33.33% - 160px);
-  height: 200px;
-  background-color: #333;
-  border-radius: 12px;
-  cursor: pointer;
-`;
